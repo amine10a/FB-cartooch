0f5b4bfd7d74d0d5dd189f6e8fc9ca75ea417896
amine10a <50719983+amine10a@users.noreply.github.com>
2020-04-14 14:57:03 +0200

    Add files via upload

---
+++ cartooch.sh
@@ -0,0 +1,15 @@
+#colors
+R='\033[1;31m'
+B='\033[1;34m'
+C='\033[0;36m'
+G='\033[1;32m'
+W='\033[1;37m'
+Y='\033[1;33m'
+clear
+#information
+echo -e $G 'all information in my youtube channel'
+echo -e $Y ' yt:Mr amine10a'
+termux-open-url http://www.youtube.com/Mr_amine10a
+echo -e $Y ' جيش القراصنة العربي '
+sleep 2
+python2 jb.Mr_amine10a.code
